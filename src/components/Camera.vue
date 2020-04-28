0dd779a54f393f8385583bf597f698a14547baaa
Adrian Rosario <adrian@factorial.io>
2020-04-28 22:42:08 +0200

    WIP: test without error page

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,49 +1,44 @@
 <template>
   <div>
-    <template v-if="!accessDenied">
-      <video
-        ref="video"
-        class="absolute inset-0 object-fill w-screen h-screen"
-      ></video>
-      <canvas
-        ref="canvas"
-        class="absolute inset-0 z-10 object-fill w-screen h-screen"
-      ></canvas>
-      <div class="absolute bottom-0 z-20 w-full mb-8 sm:mb-4">
-        <div class="flex justify-center">
-          <div class="relative inline-block">
-            <select
-              v-model="selectedCamera"
-              class="block w-full px-4 py-2 pr-8 leading-tight bg-white border border-gray-400 rounded shadow appearance-none hover:border-gray-500 focus:outline-none focus:shadow-outline"
+    <video
+      ref="video"
+      class="absolute inset-0 object-fill w-screen h-screen"
+    ></video>
+    <canvas
+      ref="canvas"
+      class="absolute inset-0 z-10 object-fill w-screen h-screen"
+    ></canvas>
+    <div class="absolute bottom-0 z-20 w-full mb-8 sm:mb-4">
+      <div class="flex justify-center">
+        <div class="relative inline-block">
+          <select
+            v-model="selectedCamera"
+            class="block w-full px-4 py-2 pr-8 leading-tight bg-white border border-gray-400 rounded shadow appearance-none hover:border-gray-500 focus:outline-none focus:shadow-outline"
+          >
+            <option value="" disabled>Select your camera</option>
+            <option
+              v-for="camera in cameraList"
+              :value="camera"
+              :key="camera.id"
+              >{{ camera.label }}</option
             >
-              <option value="" disabled>Select your camera</option>
-              <option
-                v-for="camera in cameraList"
-                :value="camera"
-                :key="camera.id"
-                >{{ camera.label }}</option
-              >
-            </select>
-            <div
-              class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none"
+          </select>
+          <div
+            class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none"
+          >
+            <svg
+              class="w-4 h-4 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
             >
-              <svg
-                class="w-4 h-4 fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                />
-              </svg>
-            </div>
+              <path
+                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+              />
+            </svg>
           </div>
         </div>
       </div>
-    </template>
-    <h1 v-else>
-      Access to the camera is needed for this app to function.
-    </h1>
+    </div>
   </div>
 </template>
 
